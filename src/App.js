0a34d656b96cf2e2256e607de30a9209220719d1
Osvaldo <osvaldoorozcorod@gmail.com>
2024-04-11 21:05:06 -0600

    build script modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import CodeMonkeyLogo from './imagenes/Code_Monkey.png'
+import ListaDeTareas from './componentes/ListaDeTareas.jsx'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aplicacion-tareas">
+      <div className='codemonkey-logo-contenedor'>
+        <img 
+        src={CodeMonkeyLogo }
+        className='codemonkey-logo' 
+        />
+      </div>
+
+      <div className='tareas-lista-principal'>
+        <h1>Mis tareas</h1>
+        <ListaDeTareas />
+      </div>
+
+
     </div>
   );
 }
